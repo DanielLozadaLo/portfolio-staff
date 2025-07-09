4a330d22cb7922b3def8e74c459e279331056206
Jerson Daniel Lozada Londoño <llozadadaniel2006@gmail.com>
2025-07-09 16:47:50 -0500

    Refactor project configuration and add mock files for testing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
   },
   moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(svg|jpg|png|gif|webp|ico|bmp)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js' 
+  } 
 };
