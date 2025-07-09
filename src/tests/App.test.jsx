3c439ef1108758b0aeb2dad6d594a0a91bae7835
Jerson Daniel Lozada Londoño <llozadadaniel2006@gmail.com>
2025-07-09 16:21:57 -0500

    Unit test creation

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('muestra el nombre completo en el encabezado', () => {
+  render(<App />);
+  expect(screen.getByText(/Jerson Daniel Lozada/i)).toBeInTheDocument();
+});
